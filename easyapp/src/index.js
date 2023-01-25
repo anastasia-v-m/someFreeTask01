9cd3a07fc6d326fd2fdb0b1bfecc0303a237bbc4
anastasia-v-m <matushevskayaanastasia@gmail.com>
2023-01-25 21:44:12 +0300

    feat: header

--- easyapp/src/index.js
+++ easyapp/src/index.js
@@ -1,22 +1,7 @@
-// import image from './images/lazy.png';
-
 import fn from './components/mouseeffects.js';
-
-// const createImage = (src) => new Promise((res, rej) => {
-//   const img = new Image();
-//   img.onload = () => res(img);
-//   img.onerror = rej;
-//   img.src = src;
-// });
-
-// async function render() {
-//   const subHeader = document.createElement('h2');
-//   subHeader.innerHTML = 'This elements was created by js';
-//   const myImage = await createImage(image);
-//   document.body.appendChild(subHeader);
-//   document.body.appendChild(myImage);
-// }
-
-// render();
+import './styles/reset.scss';
+import './styles/commonStyles.scss';
+import './styles/buttonStyles.scss';
+import './styles/headerStyles.scss';
 
 fn();
